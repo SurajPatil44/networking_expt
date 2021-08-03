97459ab8f3bf846ba870b8dccd3399f1f8159be2
SurajPatil44 <sdpatil@qti.qualcomm.com>
2021-08-03 19:35:00 +0530

    added client

---
+++ network_stream/simple_client.py
@@ -0,0 +1,66 @@
+import socket
+import struct
+import random
+import select
+
+def recv_all(conn,total_len):
+    message = bytearray()
+    while True:
+        chunk = conn.recv(total_len)
+        if chunk == b'':
+            break
+        message += chunk
+        if len(message) >= total_len:
+            return message
+
+def send_message(conn,message):
+    ## prepare len
+    slen = len(message)
+    slen = struct.pack('i',slen)
+    print(f"sending {slen} ")
+    conn.send(slen)
+    print(f"sending message")
+    conn.send(message)
+
+
+if __name__ == "__main__":
+    try:
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.connect(('localhost',8888))
+        connections = [sock]
+        tot_pack = 0
+        N = 100
+
+        while True and tot_pack <= N:
+
+            read,write,exc = select.select(connections,connections,connections,0)
+
+            for conn in read:
+                if conn == sock:
+                    read_len = recv_all(conn,4)
+                    read_len = struct.unpack('i',read_len)
+                    message = recv_all(conn,read_len)
+                    print(message)
+                else:
+                    pass
+
+            for conn in write:
+                if conn == sock:
+                    send_message(conn,b'n'*random.randint(20,40))
+                    tot_pack += 1
+                    print(f"sent {tot_pack} packets")
+                else:
+                    pass
+
+            for conn in exc: 
+                print(f"exception on {conn}")
+                ##conn.shutdown()
+                conn.close()
+                break
+
+    except Exception as E:
+        print(E)
+        if sock:
+            ##sock.shutdown(5)
+            sock.close()
+
